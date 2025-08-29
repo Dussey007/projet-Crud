036472d307d33e4d24743a3d6b03caeea877d600
Dussey <regisdussey@gmail.com>
2025-08-29 17:49:51 +0200

    exercice4 commit of JS

---
+++ cours1/exo4/cours.js
@@ -0,0 +1,11 @@
+//Condition if
+let x = 5;
+let y = -2;
+
+if (x>=3 && x<=7){
+    document.getElementById("p1").innerHTML = 'x est compris entre 3 et 7!';
+}
+
+if (y>=-6 || y<=-3){
+    document.getElementById("p2").innerHTML = 'y est trictement superieur!';
+}
