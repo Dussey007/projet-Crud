91a46b6b1748fcf19d3b749ac74ce0f798978b7f
Dussey <regisdussey@gmail.com>
2025-08-29 10:41:32 +0200

    second commit of JS  exercices

--- cours1/exercice1/cours.js
+++ cours1/exercice1/cours.js
@@ -1,5 +1,8 @@
 let bonjour = document.getElementById('b1');
 let ajouter = document.getElementById('b2');
+let a = 2;
+let x = 4;
+let y = 3;
 
 bonjour.addEventListener('click', alerte);
 ajouter.addEventListener('click', ajout);
@@ -13,3 +16,16 @@ function ajout(){
     para.textContent="texte ajouter";
     document.body.appendChild(para);
 }
+// Operateur arithmetique
+c = a + x;
+d = x % 2;
+e = x * a;
+f = y / x;
+
+alert(
+    '\nc contient '+ c +
+    '\nd contient '+ d+
+    '\ne contient '+ e+
+     '\nf contient '+ f
+);
+
---
+++ cours1/exo2/cours.js
@@ -0,0 +1,8 @@
+//CONCATENATION
+a = 24;
+x = 'Bonjour j\'ai: ';
+y = 'ans';
+
+alert(`${x} 
+    ${a} 
+    ${y} merci `)
