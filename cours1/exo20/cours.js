c7d333ffa9539aabe03498885aa12b16409b6c3e
Dussey <regisdussey@gmail.com>
2025-09-01 16:34:25 +0000

    exercice6-20 commit of JS

--- cours1/exo20/cours.js
+++ cours1/exo20/cours.js
@@ -1,17 +1,5 @@
-let b1 = document.getElementById('b1');
-let b2 = document.getElementById('b2');
-let b3 = document.getElementById('b3');
-
-b1.addEventListener('click', recharge);
-b2.addEventListener('click', assigne);
-b3.addEventListener('click', remplace);
-
-function recharge() {
-    location.reload();
-}
-function assigne() {
-    location.assign('http://127.0.0.1:5500/exo16/cours.html');
-}
-function remplace() {
-    location.replace('http://127.0.0.1:5500/exo17/cours.html');
-}
+document.getElementById('p1').innerHTML =
+    'Dimensions totales de l\'écran : ' + screen.width + '*' + screen.height +
+    '<br>Surface disponible : ' + screen.availWidth + '*' + screen.availHeight +
+    '<br>Palette de couleur : ' + screen.colorDepth +
+    '<br>Résolution : ' + screen.pixelDepth;
