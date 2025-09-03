21a6b0693ee89db1915209372e7c0b4f819367e7
Dussey <regisdussey@gmail.com>
2025-09-03 15:11:40 +0000

    exercice34-40 commit of JS

--- cours1/exo40/cours.js
+++ cours1/exo40/cours.js
@@ -1,9 +1,20 @@
+//closures des fonctions
 let pierre = { name: 'Pierre' };
 let mathilde = { name: 'Mathilde' };
+let nom = 'Junior';
 
 function disBonjour() {
     alert('Bonjour ' + this.name);
 }
-
 pierre.bonjour = disBonjour;
 mathilde.bonjour = disBonjour;
+
+function infos() {
+    let age = 19;
+    function affiche() {
+        return nom + 'est age de' + age + 'ans';
+    }
+    return affiche;
+}
+
+alert(infos());
